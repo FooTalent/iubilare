fb27512bb1b6463524f123f606fd97e9c8b60e11
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-03-08 09:45:47 -0300

    feature/navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './views/Home'
-
+import Navbar from './components/navbar/Navbar';
+ 
 
 function App() {
 
 
   return (
     <>
+        <Navbar></Navbar>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
           <Route path="" element=""></Route>
