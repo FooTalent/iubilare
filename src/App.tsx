178432f5e638d45404e7cbc060f36c3950eda7d6
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-03-08 11:30:34 -0300

    feature/whatsapp button

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './views/Home'
 import Navbar from './components/navbar/Navbar';
- 
 //import Home from './views/Home'
 import Footer from './views/Footer'
+import WhatsappButton from './components/whatsappButton/WhatsappButton';
 
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
   return (
     <>
         <Navbar></Navbar>
+        <WhatsappButton/>
         <Routes>
           <Route path="/" element={<Footer/>}></Route>
           <Route path="" element=""></Route>
---
+++ src/components/whatsappButton/WhatsappButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const WhatsappButton = () => {
+  const containerStyle = 'fixed bottom-4 right-4'; // Adjust as needed
+  const buttonStyle = 'p-2 bg-green-500 rounded-full cursor-pointer';
+  const imageStyle = 'w-6 h-6';
+  const handleButtonClick = () => {
+    window.open('https://api.whatsapp.com/send/?phone=573008659238&text&type=phone_number&app_absent=0', '_blank');
+  };
+
+  return (
+    <div className={containerStyle}>
+      <button className={buttonStyle} onClick={handleButtonClick}>
+        <img src="./whatsappbutton.png" alt="WhatsApp Icon" className={imageStyle} />
+      </button>
+    </div>
+  );
+};
+
+export default WhatsappButton;
