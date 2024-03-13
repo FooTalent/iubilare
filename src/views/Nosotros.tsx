d91cc455bef14c5d10a7667618e8078b5e2e74a7
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-03-13 20:15:49 -0300

    Feature/ cards Table Version

--- src/views/Nosotros.tsx
+++ src/views/Nosotros.tsx
@@ -16,10 +16,20 @@ export default function Nosotros () {
                     <h1 className="font-semibold text-[25px]" >Equipo de Trabajo</h1>
                 </div>
 
+                    {/* Separador 16px Para Seccion Tablet*/}
+                    <div className="md:min-h-[16px]"></div>
+                    <div className="hidden md:flex md:flex-row md:justify-center md:gap-3">
+                        <CardNosotros name={"Marcelo Visceglie"} status={"Fundador"} image={"./nosotros/image-1.png"} className="bg-background-violet shadow-avatar-shadow" />
+                        <CardNosotros name={"Cristian Pavlovsky"} status={"Fundador"} image={"./nosotros/image-1.png"} className="bg-background-violet shadow-avatar-shadow"/>
+                        <CardNosotros name={"Dr. Federico Sallustio"} status={"Abogado"} image={"./nosotros/image-1.png"} className="bg-white shadow-avatar-shadow text-black" />
+                        <CardNosotros name={"Dr. Federico Sallustio"} status={"Abogado"} image={"./nosotros/image-1.png"} className="bg-white shadow-avatar-shadow text-black" />
+                    </div>
+
+
             {/* Separador 16px */}
             <div className="min-h-[16px]"></div>
 
-                <div>
+                <div className="px-[16px] md:hidden">
                     <h3 className="text-[20px]">Fundadores</h3>
                     {/* Separador 15px */}
                     <div className="min-h-[15px]"></div>
@@ -33,14 +43,15 @@ export default function Nosotros () {
                 <div className="min-h-[48px]"></div>
 
                 {/* Imagen separador */}
-                <div className="px-[16px] flex justify-center" >
+                <div className="px-[16px] flex justify-center md:hidden" >
                     <img src="./divider.png" alt="" />
                 </div>
 
                 {/* Separador 48px */}
-                <div className="min-h-[48px]"></div>
+                <div className="min-h-[48px] md:hidden"></div>
 
-                <div className="px-[16px]">
+                {/* Seccion Abogados */}
+                <div className="px-[16px] md:hidden">
                     <h3 className="text-[20px]">Abogados</h3>
                     {/* Separador 15px */}
                     <div className="min-h-[15px]"></div>
@@ -54,9 +65,10 @@ export default function Nosotros () {
                 </div>
 
                 {/* Separador 48px */}
-                <div className="min-h-[48px]"></div>
+                <div className="min-h-[48px] md:hidden"></div>
 
-                <div className="px-[16px]">
+                {/* Seccion Secretarias */}
+                <div className="px-[16px]  md:hidden">
                     <h3 className="text-[20px]">Secretarias</h3>
                     {/* Separador 15px */}
                     <div className="min-h-[15px]"></div>
@@ -67,8 +79,10 @@ export default function Nosotros () {
                 </div>
 
                 {/* Separador 48px */}
-                <div className="min-h-[48px]"></div>
-                <div>
+                <div className="min-h-[48px]  md:hidden"></div>
+
+                {/* Seccion Ejecutivos */}
+                <div className="px-[16px]  md:hidden">
                     <h3 className="text-[20px]">Ejecutivos en Gestión Previsional</h3>
                     {/* Separador 15px */}
                     <div className="min-h-[15px]"></div>
@@ -79,8 +93,10 @@ export default function Nosotros () {
                 </div>
 
                 {/* Separador 48px */}
-                <div className="min-h-[48px]"></div>
-                <div>
+                <div className="min-h-[48px] md:hidden"></div>
+
+                {/* Seccion Asistentes */}
+                <div className="px-[16px] md:hidden">
                     <h3  className="text-[20px]">Asistentes legales</h3>
                     {/* Separador 15px */}
                     <div className="min-h-[15px]"></div>
@@ -91,10 +107,10 @@ export default function Nosotros () {
                 </div>
 
                 {/* Separador 48px */}
-                <div className="min-h-[48px]"></div>
+                <div className="min-h-[48px] md:hidden"></div>
 
-
-                <div className="px-[16px]">
+                {/* Seccion Mision */}
+                <div className="px-[16px] md:px-[41px]">
                     <h1 className="font-semibold text-[25px]" >Misión</h1>
                     {/* Separador 15px */}
                     <div className="min-h-[15px]"></div>
@@ -106,7 +122,8 @@ export default function Nosotros () {
                 {/* Separador 48px */}
                 <div className="min-h-[48px]"></div>
 
-                <div className="px-[16px]">
+                {/* Seccion Vision */}
+                <div className="px-[16px] md:px-[41px]">
                     <h1 className="font-semibold text-[25px]" >Visión</h1>
                     {/* Separador 15px */}
                     <div className="min-h-[15px]"></div>
@@ -118,7 +135,8 @@ export default function Nosotros () {
                 {/* Separador 48px */}
                 <div className="min-h-[48px]"></div>
 
-                <div className="px-[16px]">
+                {/* Seccion Mapa */}
+                <div className="px-[16px] md:px-[41px]">
                     <h1 className="font-semibold text-[25px]" >Donde encontrarnos</h1>
                         {/* Separador 15px */}
                         <div className="min-h-[15px]"></div>
