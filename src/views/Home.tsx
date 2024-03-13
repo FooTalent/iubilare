306bf364f120a8ffa3b5ddf27798ac0a6108521b
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-03-13 12:45:45 -0300

    fix/footer

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -32,8 +32,6 @@ export default function Home() {
                 Testomonios
                 Formulario
             </div>
-            <footer className="text-center py-4">
-            </footer>
         </div>
     )
 };
