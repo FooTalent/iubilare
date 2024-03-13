de457edd84fe9202f41dafce33babbec173b94e4
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-03-13 17:56:01 -0300

    feature/ nosotros cards

--- src/components/cardNosotros/CardNosotros.tsx
+++ src/components/cardNosotros/CardNosotros.tsx
@@ -1,16 +1,17 @@
-
-
-export default function CardNosotros () {
-
-
+interface CardNosotrosProps {
+    name: string;
+    status: string;
+    image: string;
+  }
+  
+  const CardNosotros: React.FC<CardNosotrosProps> = ({ name, status, image }) => {
     return (
-        <>
-            <div className="bg-background-violet w-[160px] h-[200px] rounded-md shadow-2xl pt-[16px] px-[16px]">
-                    <img src="./nosotros/image-1.png" alt="" />
-                    <p className="text-white text-[15px]">Marcelo Visceglie</p>
-                    <p className="text-white text-[12px]">Fundador</p>
-            </div>
-        </>
-    )
-
-}
+      <div className="bg-background-violet w-[160px] h-[200px] rounded-md shadow-2xl pt-[16px] px-[16px]">
+        <img src={image} alt="" />
+        <p className="text-white text-[15px]">{name}</p>
+        <p className="text-white text-[10px]">{status}</p>
+      </div>
+    );
+  };
+  
+  export default CardNosotros;
--- src/views/Nosotros.tsx
+++ src/views/Nosotros.tsx
@@ -1,6 +1,5 @@
+import CardAbogados from "../components/cardNosotros/CardAbogados"
 import CardNosotros from "../components/cardNosotros/CardNosotros"
-
-
 
 
 export default function Nosotros () {
@@ -16,16 +15,42 @@ export default function Nosotros () {
                 <div>
                     <h1 className="font-semibold text-[25px]" >Equipo de Trabajo</h1>
                 </div>
+
+            {/* Separador 16px */}
+            <div className="min-h-[16px]"></div>
+
                 <div>
                     <h3 className="text-[20px]">Fundadores</h3>
+                    {/* Separador 15px */}
+                    <div className="min-h-[15px]"></div>
                     <div className="flex flex-row justify-center gap-3">
-                        <CardNosotros/>
-                        <CardNosotros/>
+                        <CardNosotros name={"Marcelo Visceglie"} status={"Fundador"} image={"./nosotros/image-1.png"} />
+                        <CardNosotros name={"Cristian Pavlovsky"} status={"Fundador"} image={"./nosotros/image-1.png"}/>
                     </div>
                 </div>
-                <div>
+
+                {/* Separador 48px */}
+                <div className="min-h-[48px]"></div>
+
+                {/* Imagen separador */}
+                <div className="px-[16px] flex justify-center" >
+                    <img src="./divider.png" alt="" />
+                </div>
+
+                {/* Separador 48px */}
+                <div className="min-h-[48px]"></div>
+
+                <div className="px-[16px]">
                     <h3 className="text-[20px]">Abogados</h3>
-                    <div>Cards</div>
+                    {/* Separador 15px */}
+                    <div className="min-h-[15px]"></div>
+                    <div className=" flex justify-center gap-3">
+                        <CardAbogados name={"Dr. Federico Sallustio"} status={"Abogado"} image={"./nosotros/image-1.png"} />
+                        <CardAbogados name={"Dr. Federico Sallustio"} status={"Abogado"} image={"./nosotros/image-1.png"} />
+                    </div>
+                    <div>
+
+                    </div>
                 </div>
                 <div>
                     <h3 className="text-[20px]">Secretarias</h3>
