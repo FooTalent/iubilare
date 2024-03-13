b18042a68d7e664550b1a83bcc2b30335238899e
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-03-13 08:00:04 -0300

    feature/ cards

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,11 +1,32 @@
+import React from "react";
 
-export default function Card (){
-
-
-    return (
-        <>
-        </>
-    )
-
-
+interface CardProps {
+  name: string;
+  age: string;
+  description: string;
+  imageUrl: string;
 }
+
+const Card: React.FC<CardProps> = ({ name, age, description, imageUrl }) => {
+  return (
+    <div className="flex flex-col justify-center items-center bg-[#e8e8e8] w-[208px] h-[233.92px] pt-[18px] pb-[20px] px-[8px] rounded-md">
+      <div className="flex flex-col justify-center items-center w-[192px] h-[171.73px] overflow-hidden">
+        <div className="flex flex-col justify-center items-center w-[95px] h-[84.73px] mb-[13px]">
+          <div className="w-[48.73px] h-[48.73px] mb-[10px]">
+            <img className="rounded-full" src={imageUrl} alt="" />
+          </div>
+          <div>
+            <p className="text-xs">{name}</p>
+            <p className="text-xs">{age}</p>
+          </div>
+        </div>
+        <div className="w-[192px] h-[71px]">
+          <p className="text-xs line-clamp-4">{description}</p>
+        </div>
+      </div>
+      <div>stars</div>
+    </div>
+  );
+};
+
+export default Card;
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,20 @@
-import Form from "../components/form/Form"
+import Card from "../components/card/Card";
+import Form from "../components/form/Form";
 
 
 export default function Home() {
+ 
+    const cards = [
+        { id: 1, name: "Lorena Ontivero", age: "53 años",description: "Valoré mucho el asesoramiento y  permanente contacto con Cristian, y agradezco nuevamente tu trabajo eficiente y el de todo tu equipo en general", imageUrl: "https://via.placeholder.com/300" },
+        { id: 2, name: "Lorena Ontivero", age: "53 años",description: "Valoré mucho el asesoramiento y  permanente contacto con Cristian, y agradezco nuevamente tu trabajo eficiente y el de todo tu equipo en general", imageUrl: "https://via.placeholder.com/300" },
+        { id: 3, name: "Lorena Ontivero", age: "53 años",description: "Valoré mucho el asesoramiento y  permanente contacto con Cristian, y agradezco nuevamente tu trabajo eficiente y el de todo tu equipo en general", imageUrl: "https://via.placeholder.com/300" },
+        ];
+
+        const data = [... cards, ... cards];
 
     return (
+
+        
         <>
             <div className="text-center mb-40" >
             27 años de experiencia. Asesoramiento personalizado. 
@@ -17,10 +28,24 @@ export default function Home() {
             <div>
                 Quienes somos
             </div>
-            <div>
-                Testomonios
-                <Form></Form>
-            </div>
+
+            <div className="min-h-[108px]"></div>
+            <div><h2 className="text-xl font-semibold">Testimonios</h2></div>
+            <div className="min-h-[48px]"></div>
+
+				<div className='overflow-hidden w-full h-full'>
+					<div className='flex whitespace-nowrap animate-scroll gap-5'>
+                    {data.map((card, index) => (
+                        <Card key={index} name={card.name} age={card.age} description={card.description} imageUrl={card.imageUrl} />
+                    ))}
+					</div>
+				</div>
+
+
+
+            <div className="min-h-[106.08px]"></div>
+            <Form></Form>
+
             <footer className="text-center py-4">
             </footer>
         </>
