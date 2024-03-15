3a9ffde5e35435d1c4ca0e81a40c2a21fcb3cf34
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-03-15 10:46:05 -0300

    fix/ whatsapp button hover effect

--- src/components/whatsappButton/WhatsappButton.tsx
+++ src/components/whatsappButton/WhatsappButton.tsx
@@ -1,7 +1,7 @@
 
 const WhatsappButton = () => {
   const containerStyle = 'fixed bottom-4 right-4 z-10'; // Adjust as needed
-  const buttonStyle = 'p-2 bg-green-500 rounded-full cursor-pointer';
+  const buttonStyle = 'p-2 bg-[#439373] rounded-full cursor-pointer hover:bg-green-500';
   const imageStyle = 'w-6 h-6';
   const handleButtonClick = () => {
     window.open('https://api.whatsapp.com/send/?phone=573008659238&text&type=phone_number&app_absent=0', '_blank');
@@ -10,7 +10,7 @@ const WhatsappButton = () => {
   return (
     <div className={containerStyle}>
       <button className={buttonStyle} onClick={handleButtonClick}>
-        <img src="./whatsappbutton.png" alt="WhatsApp Icon" className={imageStyle} />
+        <img src="./whatsappButton.png" alt="WhatsApp Icon" className={imageStyle} />
       </button>
     </div>
   );
