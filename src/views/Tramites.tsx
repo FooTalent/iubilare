0635c694a2cf8364dc2b9e7fb75c55660dc9a55a
RafaelAngarita9 <rafaelang04@gmail.com>
2024-03-19 08:20:51 -0500

    divisiones_tramites

--- src/views/Tramites.tsx
+++ src/views/Tramites.tsx
@@ -5,8 +5,28 @@ export default function Tramites (){
 
 
     return (
-        <>
-        </>
-    )
+        <div className="flex flex-col h-screen">
+            {/* Primera Sección */}
+            <div className="flex-1 flex justify-center items-center bg-blue-100">
+                <p>Primera Sección</p>
+            </div>
+    
+            {/* Segunda Sección */}
+            <div className="flex-1 flex justify-center items-center bg-blue-200">
+                <p>Segunda Sección</p>
+            </div>
+    
+            {/* Tercera Sección */}
+            <div className="flex-1 flex justify-center items-center bg-blue-300">
+                <p>Tercera Sección</p>
+            </div>
+    
+            {/* Cuarta Sección */}
+            <div className="flex-1 flex justify-center items-center bg-blue-400">
+                <p>Cuarta Sección</p>
+            </div>
+        </div>
+    );
+    
 
 }
