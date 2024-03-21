45120c4c51b37f1b4c1e39405e6f90d0cd560a67
TomasD1998 <tomidistefano@hotmail.com>
2024-03-21 11:28:31 -0300

    tramites1

--- src/views/Tramites.tsx
+++ src/views/Tramites.tsx
@@ -19,12 +19,17 @@ export default function Tramites (){
                   <p className="ml-2 pl-2 text-left font-semibold text-base">Cobra menos que la jubilación mínima</p>
                 </div>
                 <div className="w-1/3">
-                  <button onClick={() => setIsExpanded(!isExpanded)} className={`bg-${isExpanded ? 'bg-white' : '747DD6'} font-semibold flex flex-row py-0.5 px-2 text-sm text-white rounded shadow-card-shadow`}>{isExpanded ? 'Ver menos' : 'Ver más'} <img className="pt-2 pl-2" src="./arrow.png" alt="arrow" /></button>
+                  <button onClick={() => setIsExpanded(!isExpanded)} className={`${isExpanded ? 'bg-white text-747DD6' : 'bg-747DD6 text-white'} font-semibold flex flex-row py-0.5 px-2 text-sm rounded shadow-card-shadow focus:outline-none`}>{isExpanded ? 'Ver menos' : 'Ver más'} <img className="pt-2 pl-2" src="./arrow.png" alt="arrow" /></button>
                 </div>
               </div>
               {isExpanded && 
               <div className="flex flex-row justify align-middle py-5 w-2/3">
-                <p className="ml-2 pl-2 text-left font-semibold text-base">Texto adicional del estado después...</p>
+                <p className="ml-2 pl-2 text-left font-semibold text-base">Un nuevo fallo ordena pagar la jubilación mínima a las Rentas Previsionales que no llegan a ese monto.
+
+Todas las personas jubiladas o pensionadas por una Compañía de Seguro de Retiro –ex AFJP- pueden mejorar su haber mensual.
+Si Usted cobra de una Compañía de Seguro de Retiro una Renta Vitalicia Previsional inferior a la jubilación mínima -conforme fallo de la CORTE SUPREMA DE JUSTICIA DE LA NACIÓN, ¨Etchart, Fernando Martín c/ANSES s/Amparos y Sumarísimos¨ -CSJN- 27/10/2015. está en condiciones de iniciar un reclamo.
+Este nuevo fallo habilita nuevos derechos.
+Quienes viven en la Patagonia disponen de una bonificación por Zona Austral.</p>
               </div>
               }
             </div>
@@ -34,7 +39,7 @@ export default function Tramites (){
                 <p className="ml-2 pl-2 text-left font-semibold text-base">Renta anterior al 2002</p>
               </div>
               <div className="w-1/3">
-                <button className="bg-747DD6 font-semibold flex flex-row py-0.5 px-2 text-sm text-white rounded shadow-card-shadow">Ver más <img className="pt-2 pl-2" src="./arrow.png" alt="arrow" /></button>
+                <button className="bg-747DD6 font-semibold flex flex-row py-0.5 px-2 text-sm text-white rounded shadow-card-shadow">Ver más <img className="pt-2 pl-2" src="./arrow.svg" alt="arrow" /></button>
               </div>
             </div>
 
