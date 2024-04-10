da3f9b72cbe0f36ff399495e6c2adb1a7329db58
Gonzalo Rodriguez <gonzalo-ezequiel@hotmail.com>
2024-04-10 17:28:49 -0500

    centrado del texto de la seccion de vision y mision

--- src/components/cardQueHacemos/cardQueHacemos.tsx
+++ src/components/cardQueHacemos/cardQueHacemos.tsx
@@ -16,7 +16,7 @@ const CardQueHacemos: React.FC<MyComponentProps> = ({
   classStyle,
 }) => (
   <div
-    className={`flex flex-col justify-evenly xl:justify-center w-[328px] h-auto xl:p-[7px]  rounded-[5px] gap-[15px] md:m-0 md:bg-transparent md:flex md:flex-row md:w-full xl:w-[1200px] items-start shadow-card-shadow ${classStyle}`}
+    className={`flex flex-col justify-evenly xl:justify-center w-[328px] h-auto xl:p-[7px]  rounded-[5px] gap-[15px] md:m-0 md:bg-transparent md:flex md:flex-row md:w-full xl:w-[1200px] items-center shadow-card-shadow ${classStyle}`}
   >
     <img
       className="w-[314px] h-[228px] mx-auto md:mx-0 rounded-md shadow md:w-[337px] xl:w-[460px] xl:h-[330px]"
